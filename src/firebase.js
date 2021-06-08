f03796600ef2d7d8dfc4908bc0990ffc2d395896
Dhawal Darji <dhawal.d7@gmail.com>
2021-06-08 13:25:29 -0400

    Navbar completed

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAxCp6lBCdn9N1_-Y2DlLWYsJseaMUZZoI",
+  authDomain: "amznclonebydhawal.firebaseapp.com",
+  projectId: "amznclonebydhawal",
+  storageBucket: "amznclonebydhawal.appspot.com",
+  messagingSenderId: "444373773921",
+  appId: "1:444373773921:web:161646f5b581aed9c20420",
+  measurementId: "G-CXFKP10DE1",
+};
